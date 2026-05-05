a2ba4320b1ae9220b2e5acd28858ab64938fc028
ritika suwal <ritikasuwal1@gmail.com>
2026-05-05 14:27:52 +0545

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <h1 className="text-4xl text-red-500">
+        Hello World
+      </h1>
+    </>
+  );
+};
+
+export default Home;
